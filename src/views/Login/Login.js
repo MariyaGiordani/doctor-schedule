d0c8e06a4b975f7c12a215b10b024fe6b6315f65
Mariya <33662721+MariyaGiordani@users.noreply.github.com>
2020-04-12 18:58:50 -0300

    Login was created together with home page

---
+++ src/views/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    console.log("Login", event.value)
+    event.preventDefault();
+  }
+
+  return (
+    <div className="login">
+        <form className="login__form" onSubmit={handleSubmit}>
+          <h3>Sign In</h3>
+
+          <div className="form-group">
+              <label>Email</label>
+              <input className="form-control" placeholder="Endereço de e-mail" autoFocus type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+          </div>
+
+          <div className="form-group">
+              <label>Senha</label>
+              <input className="form-control" placeholder="Entre senha" value={password} onChange={e => setPassword(e.target.value)} type="password"/>
+          </div>
+
+          <div className="form-group">
+              <div className="custom-control custom-checkbox">
+                  <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                  <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+              </div>
+          </div>
+
+          <button type="submit" block className="btn btn-primary btn-block" disabled={!validateForm()}>Fazer login</button>
+          <p className="forgot-password text-right">
+              Forgot <a href="#">password?</a>
+          </p>
+        </form>
+    </div>
+  );
+}
