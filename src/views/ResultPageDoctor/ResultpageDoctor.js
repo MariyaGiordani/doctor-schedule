7fbce1c0b0d94165227eb98253bce8afdca3ffe4
Mariya Giordani <mariyalukanova@hotmail.com>
2020-05-31 10:48:54 -0300

    Css changes

--- src/views/ResultPageDoctor/ResultpageDoctor.js
+++ src/views/ResultPageDoctor/ResultpageDoctor.js
@@ -5,11 +5,13 @@ import ResultDoctor from '../../components/ResultDoctor/resultDoctor';
 export default class ResultPageDoctor extends React.Component {
    render(){
     return (
-        <div className="result-doctor">
-          <div className="result-doctor__title"> 
-              <h5 className="title">Nome do Médico: {this.props.location.state.doctor.FirstName} {this.props.location.state.doctor.LastName}</h5>
+        <div className="result-doctor--background">
+          <div className="result-doctor">
+            <div className="result-doctor__title"> 
+                <h5 className="title">Nome do Médico: {this.props.location.state.doctor.FirstName} {this.props.location.state.doctor.LastName}</h5>
+            </div>
+            <ResultDoctor information={this.props.location.state}></ResultDoctor>
           </div>
-          <ResultDoctor information={this.props.location.state}></ResultDoctor>
         </div>
       )
     }
